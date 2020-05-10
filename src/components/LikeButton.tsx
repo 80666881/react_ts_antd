4997ff5eead746b3c3bc7248e4cc5601f6e8ce8e
铁腕阿童木 <87192329@163.com>
2020-05-10 22:37:59 +0800

    useRef complete

--- src/components/LikeButton.tsx
+++ src/components/LikeButton.tsx
@@ -1,20 +1,35 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect,useRef } from 'react'
 import useMousePosition from '../hooks/useMousePosition'
 const LikeButton: React.FC = () => {
     const [like, setLike] = useState(0)
     const [on, setOn] = useState(false)
-    const positions = useMousePosition()
+    const likeRef = useRef(0)
+    const didMoutRef = useRef(false)
+    const domRef = useRef<HTMLInputElement>(null)
     useEffect(() => {
         console.log('like Button effect is running');
         //告诉react渲染后执行哪些操作
         document.title = `点击了${like}次`
     },[like])
+    useEffect(()=>{
+        if(didMoutRef.current){
+            console.log('update.....');
+        }else{
+            didMoutRef.current = true
+        }
+    })
+    useEffect(()=>{
+        if(domRef && domRef.current){
+            domRef.current.focus()
+        }
+    })
     return (
         <>
-            <button onClick={() => setLike(like + 1)}>
+            <button onClick={() => {setLike(like + 1);likeRef.current++}}>
                 {like}赞
             </button>
-            <button onClick={()=>setTimeout(()=>{alert(like)},1000)}>Alert</button>
+            <input type="text" ref={domRef}/>
+            <button onClick={()=>setTimeout(()=>{alert(likeRef.current)},1000)}>Alert</button>
             <button onClick={() => setOn(!on)}>
                 {on?'on':'off'}
             </button>
